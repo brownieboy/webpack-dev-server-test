77b57d1ad35d6c887856265a1676430f334bf337
brownieboy@gmail.com <brownieboy@gmail.com>
2015-08-19 11:09:47 +1000

    Started again with config from kanban From Notes To Kanban 1.7.  Fixed problem where devtool = source-map would not start.

--- webpack.config.js
+++ webpack.config.js
@@ -3,63 +3,53 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var merge = require('webpack-merge');
 
-var TARGET = process.env.TARGET;
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 
 var common = {
-    entry: {
-        app: [path.resolve(ROOT_PATH, 'app/app')]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(ROOT_PATH, 'build'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            loaders: ['babel']
-        }]
-    }
+  entry: path.resolve(ROOT_PATH, 'app/app.jsx'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: path.resolve(ROOT_PATH, 'app')
+      }
+    ]
+  }
+  // ,
+  // plugins: [
+  //   new HtmlwebpackPlugin({
+  //     title: 'Kanban app'
+  //   })
+  // ]
 };
 
-
-if (TARGET === 'build') {
-    module.exports = merge(common, {
-        devtool: "source-map"
-            // plugins: [
-            //     new HtmlwebpackPlugin({
-            //         title: 'Webpack-dev-server test',
-            //         template: 'app/index.html',
-            //         inject: "body"
-            //     })
-            // ]
-    });
-}
-
-
-if (TARGET === 'dev') {
-    module.exports = merge(common, {
-        devtool: 'eval',
-        module: {
-            loaders: [{
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                loaders: ['react-hot', 'babel']
-            }]
-        },
-        devServer: {
-            colors: true,
-            historyApiFallback: true,
-            hot: true,
-            inline: true,
-            progress: true
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin() // Needs to be here, even with devServer.hot set to true
-        ]
-    });
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['react-hot', 'babel'],
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    },
+    devServer: {
+      colors: true,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
 }
---
+++ webpack.config.old.js
@@ -0,0 +1,65 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+    entry: {
+        app: [path.resolve(ROOT_PATH, 'app/app.jsx')]
+    },
+    // resolve: {
+    //     extensions: ['', '.js', '.jsx']
+    // },
+    output: {
+        path: path.resolve(ROOT_PATH, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx$/,
+            include: path.resolve(ROOT_PATH, 'app'),
+            loaders: ['babel']
+        }]
+    }
+};
+
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {
+        devtool: "source-map"
+            // plugins: [
+            //     new HtmlwebpackPlugin({
+            //         title: 'Webpack-dev-server test',
+            //         template: 'app/index.html',
+            //         inject: "body"
+            //     })
+            // ]
+    });
+}
+
+
+if (TARGET === 'dev') {
+    module.exports = merge(common, {
+        devtool: 'source-map',
+        module: {
+            loaders: [{
+                test: /\.jsx$/,
+                loaders: ['react-hot', 'babel'],
+                include: path.resolve(ROOT_PATH, 'app')
+            }]
+        },
+        devServer: {
+            colors: true,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin() // Needs to be here, even with devServer.hot set to true
+        ]
+    });
+}
